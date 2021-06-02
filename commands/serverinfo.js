4858475d9912e53c2600abf29966dc67aae80302
Lordmoose213 <lucaseaton05@icloud.com>
2021-06-02 11:47:20 -0400

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+	// command name
+	name: 'serverinfo',
+
+	// command description
+	usage: 'gets info on a server',
+
+	// sends server name and total member count
+	execute(message) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+
+		return;
+	},
+};
