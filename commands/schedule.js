9a01dd7e521874ce60c12ede12199293416e149c
Lordmoose213 <lucaseaton05@icloud.com>
2021-06-05 23:47:37 -0400

    Remade f!schedule command

--- commands/schedule.js
+++ commands/schedule.js
@@ -9,36 +9,11 @@ module.exports = {
     // The code of the command
     execute(message, args) {
         // The content of the command
+
         let serverID = message.guild.id;
-        let firnandoID = '642203556312776714';
-        let yruuID = '827570784930365480';
-        let uuccID = '773618438135873558';
-        let testServerID = '742876189009641562'
-        if (serverID === firnandoID) { 
-        message.channel.send(
-            "**Sunday**: \n" +
-            "**Monday**:\n" +
-            "**Tuesday**: Discussion night at 8:00 pm EST\n" +
-            "**Wednesday**: Council meeting at 5:00 pm EST\n" +
-            "**Thursday**:\n" +
-            "**Friday**: Weekly Games night or monthly movie night at 8:00pm EST\n" +
-            "**Saturday**: Monthly Minecraft event");
-            return
-        }
-        
-        else if (serverID === yruuID) {
-        message.channel.send
-        ('**Sunday**: Biweekly YRUU meeting');
-        return
-        }
+        let schedule = require("../schedules/" + serverID + ".json");
 
-        else if (serverID === testServerID) {
-            message.channel.send
-            ('**Sunday**: Schedule Test Successful');
-            return
-            }
-
-        else message.channel.send('No schedule exists for this server');
+        message.channel.send(schedule);
         return
 
     },
